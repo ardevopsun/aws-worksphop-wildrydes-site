361cab8765ac1a624aa75fc4066cd9e4b636cf22
ardevopsun1 <arundevops22@gmail.com>
2025-06-24 19:48:44 +0530

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_7C5DZvz08', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5bh23o8q3tn449dej5cduthsv6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://lwdz6uedde.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
